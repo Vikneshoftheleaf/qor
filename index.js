81b4f70b2c57c445b60a68f13ba5bc98f574ce3a
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2023-08-27 10:25:13 +0530

    alpha

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const qrcode = require('qrcode');
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/',(res,req)=>{
+    res.sendFile("index.html");
+})
+
+app.get('/generate', async (req, res) => {
+    const url = req.query.url;
+    try {
+      const qrCode = await qrcode.toDataURL(url);
+      const qdata = {
+        "qrcode": qrCode
+      };
+      res.render('gen', qdata)
+    } catch (error) {
+      res.status(500).send('Error generating QR code');
+    }
+  });
+  
+
+app.listen(port,()=>{
+    console.log(`Server is listening at ${port}`);
+})
---
+++ public/script.js
@@ -0,0 +1,4 @@
+const genbtn = document.getElementById('gen');
+const qrUrl = document.getElementById('qrUrl');
+const qrcon = document.getElementById('qrcon');
+const qrcode = require('qrcode')
